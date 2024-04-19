c8653f8c2214f396513be2e8b5d2d02874de6cb0
Onimai！ <93068569+Onimaimai@users.noreply.github.com>
2024-04-19 18:21:13 +0800

    Update fetch_dynamic.py

--- nonebot_plugin_bilichat/lib/fetch_dynamic.py
+++ nonebot_plugin_bilichat/lib/fetch_dynamic.py
@@ -20,25 +20,6 @@ from ..model.exception import AbortError
 from ..optional import capture_exception
 from ..subscribe.manager import SubscriptionSystem
 
-DYNAMIC_TYPE_MAP = {
-    "DYNAMIC_TYPE_FORWARD": DynamicType.forward,
-    "DYNAMIC_TYPE_WORD": DynamicType.word,
-    "DYNAMIC_TYPE_DRAW": DynamicType.draw,
-    "DYNAMIC_TYPE_AV": DynamicType.av,
-    "DYNAMIC_TYPE_ARTICLE": DynamicType.article,
-    "DYNAMIC_TYPE_MUSIC": DynamicType.music,
-}
-
-DYNAMIC_TYPE_IGNORE = {
-    "DYNAMIC_TYPE_AD",
-    "DYNAMIC_TYPE_LIVE",
-    "DYNAMIC_TYPE_LIVE_RCMD",
-    "DYNAMIC_TYPE_BANNER",
-    DynamicType.ad,
-    DynamicType.live,
-    DynamicType.live_rcmd,
-    DynamicType.banner,
-}
 
 
 async def fetch_last_dynamic(up: SearchUp) -> Union[Dynamic, None]:
